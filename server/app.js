f064071496ad4ac7b11994341177ae157a6f2040
Ali Sattar <alirsattar@gmail.com>
2018-07-31 17:26:12 -0400

    App.js adjusted to make userRoutes work

--- server/app.js
+++ server/app.js
@@ -9,7 +9,15 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const cors         = require('cors');
 
+const passport     = require('passport');
+const session      = require('express-session');
+const passportSetup = require('./config/passport');
+
+const app = express();
+
+// SETTING UP MONGOOSE CONNECTINO TO MONGODB
 mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/server', {useMongoClient: true})
@@ -19,10 +27,10 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
+// SETTING UP DEBUG
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -38,21 +46,40 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
+// SETTING UP PATH
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+// SETTING UP SESSION
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
 
+// INITIALIZING PASSPORT AND SESSION
+app.use(passport.initialize());
+app.use(passport.session());
+
+passportSetup(passport);
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+// SETTING UP CORS
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:4200']
+}));
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api', userRoutes);
 
 
 module.exports = app;
