b14039d0238f204a14e14c6fb4fb5eefd9df27b8
Ali Sattar <alirsattar@gmail.com>
2018-08-02 17:38:49 -0400

    Tidied up organization model file + started seeds

--- server/models/organization.js
+++ server/models/organization.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-
+const mongoose    = require('mongoose');
+const Schema      = mongoose.Schema;
 const organizationSchema = new Schema({
-  name: {type: String, required: true, unique: true},
-  address: {type: String, required: true},
-  email: {type: String, required: true, unique: true},
-  phoneNumber: {type: Number, required: true, unique: true},
-  description: String,
-  logo: String,
-  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
-  staff: [{
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    role: {type: String, enum: ['ADMIN', 'VOLUNTEER']}
-  }], 
-  events: [{type: Schema.Types.ObjectId, ref: 'Event'}]
+  name:           {type: String, required: true, unique: true},
+  address:        {type: String, required: true},
+  email:          {type: String, required: true, unique: true},
+  phoneNumber:    {type: Number, required: true, unique: true},
+  description:    String,
+  logo:           String,
+  reviews:        [{type: Schema.Types.ObjectId, ref: 'Review'}],
+  staff:          [{
+                    user: {type: Schema.Types.ObjectId, ref: 'User'},
+                    role: {type: String, enum: ['ADMIN', 'VOLUNTEER']}
+                  }], 
+  events:         [{type: Schema.Types.ObjectId, ref: 'Event'}]
 });
-
 const Organization = mongoose.model('Organization', organizationSchema);
 module.exports = Organization;
