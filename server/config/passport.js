42e8e6f52be0987c35c8a6a7a32d0c5dce97d1c3
Ali Sattar <alirsattar@gmail.com>
2018-08-02 12:10:26 -0400

    User profile CRUD + authentication routes finished and working

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,41 +2,36 @@ const LocalStrategy = require('passport-local').Strategy;
 const User          = require('../models/user');
 const bcrypt        = require('bcryptjs');
 
-module.exports = function (passport) { // <-- THIS IS EQUIVALENT TO PUTTING MODULE.EXPORTS = PASSPORT,
-                                       //     AND MAKING THIS "FUNCTION PASSPORT() = { ... }"
+module.exports = function (passport) {              // <-- THIS IS EQUIVALENT TO PUTTING MODULE.EXPORTS = PASSPORT,
+  passport.use(new LocalStrategy(                   //     AND MAKING THIS "FUNCTION PASSPORT() = { ... }"
 
-  passport.use(new LocalStrategy((username, password, next) => {
-    User.findOne({ username }, (err, foundUser) => {
+    {usernameField: 'email'},                           // <-- THIS NEEDED TO BE CHANGED TO ACCOUNT IN PASSPORT
+    (email, password, next) => {                        //     LOCAL STRATEGY FOR USING EMAIL INSTEAD OF USERNAME
+    User.findOne({ email }, (err, foundUser) => {
       if (err) {
         next(err);
         return;
       }
-
       if (!foundUser) {
-        next(null, false, { message: 'Incorrect username' });
+        next(null, false, { message: 'Incorrect email' });
         return;
       }
-
       if (!bcrypt.compareSync(password, foundUser.password)) {
         next(null, false, { message: 'Incorrect password' });
         return;
       }
-
       next(null, foundUser);
     });
   }));
-
   passport.serializeUser((loggedInUser, cb) => {
     cb(null, loggedInUser._id);
   });
-
   passport.deserializeUser((userIdFromSession, cb) => {
     User.findById(userIdFromSession, (err, userDocument) => {
       if (err) {
         cb(err);
         return;
       }
-
       cb(null, userDocument);
     });
   });
