ba2b721f991e17ee755f8f66f08471caf986870d
Ali Sattar <alirsattar@gmail.com>
2018-08-03 15:39:25 -0400

    Started to add user create org conditional, but we decided against it

--- server/models/user.js
+++ server/models/user.js
@@ -15,7 +15,8 @@ const userSchema = new Schema({
     profilePicture:       {type: String, default: 'https://vignette.wikia.nocookie.net/shingekinokyojin/images/c/c0/Anonymous_User.jpg/revision/latest?cb=20160312154300'},
     phoneNumber:          Number,
     orgAdmin:             [{type: Schema.Types.ObjectId, ref: 'Organization', default: []}],
-    events:               [{type: Schema.Types.ObjectId, ref: 'Event', default: []}]
+    events:               [{type: Schema.Types.ObjectId, ref: 'Event', default: []}],
+    organizations:        [{type: Schema.Types.ObjectId, ref: 'Organization', default: []}]
 },
 {  
     usePushEach: true
