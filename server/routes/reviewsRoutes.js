4ee397af88358c42951b08f557aff4bbe225c673
Ali Sattar <alirsattar@gmail.com>
2018-08-03 15:47:46 -0400

    Added route to get all reviews, previously edited User model to include empty array of organizations to start with

--- server/routes/reviewsRoutes.js
+++ server/routes/reviewsRoutes.js
@@ -11,9 +11,9 @@ router.post('/create', (req, res, next) => {
     orgID:      req.body.orgID
   });
   newReview.save(error => {
-    if(error)                  {res.status(400).json(error);} 
-    else if(newReview.eventID) {res.redirect(307, `/api/events/${newReview.eventID}/addReview/${newReview._id}`)}
-    else                       {res.redirect(307, `/api/orgs/${newReview.orgID}/addReview/${newReview._id}`)}
+    if(error)                   {res.status(400).json(error);} 
+    else if(newReview.eventID)  {res.redirect(307, `/api/events/${newReview.eventID}/addReview/${newReview._id}`)}
+    else                        {res.redirect(307, `/api/orgs/${newReview.orgID}/addReview/${newReview._id}`)}
   });
 });
 
@@ -24,9 +24,9 @@ router.post('/edit/:reviewID', (req, res, next) => {
     const reviewID = req.params.reviewID;
     if(!req.body.content || req.body.content === '') {res.status(400).json({message: 'Review content is required'}); return;}
     Review.findByIdAndUpdate(reviewID, req.body, {new: true}, (err, theReview) => {
-        if(err)             {res.status(400).json(err)}
-        else if(!theReview) {res.status(400).json({message: 'Review does not exist'})}
-        else                {res.status(200).json(theReview)}
+        if(err)                 {res.status(400).json(err)}
+        else if(!theReview)     {res.status(400).json({message: 'Review does not exist'})}
+        else                    {res.status(200).json(theReview)}
     });
 });
 
@@ -36,21 +36,30 @@ router.post('/edit/:reviewID', (req, res, next) => {
 router.post('/delete/:reviewID', (req, res, next) => {
     const reviewID = req.params.reviewID;
     Review.findByIdAndRemove(reviewID, (err, theReview) => {
-        if(err)             {res.status(400).json(err)}
-        else if(!theReview) {res.status(400).json({message: 'Review does not exist'})}
-        else                {res.status(200).json({message: 'Success'})}
+        if(err)                 {res.status(400).json(err)}
+        else if(!theReview)     {res.status(400).json({message: 'Review does not exist'})}
+        else                    {res.status(200).json({message: 'Success'})}
     });
 });
 
 // ---------------------------------------------------------------------------------------------------------
   
 // GET ROUTE FOR GETTING ONE REVIEW
+router.get('/allreviews', (req, res, next) => {
+    Review.find((err, theReviews) => {
+    if(err)                     {res.status(400).json(err)}
+    else if (!theReviews)       {res.status(400).json({message: 'Reviews not found'})}
+    else                        {res.status(200).json(theReviews)}
+    });
+});
+
+// GET ROUTE FOR GETTING ONE REVIEW
 router.get('/:reviewID', (req, res, next) => {
     const reviewID = req.params.reviewID;
     Review.findById(reviewID, (err, theReview) => {
-    if(err)               {res.status(400).json(err)}
-    else if (!theReview)  {res.status(400).json({message: 'Review does not exist'})}
-    else                  {res.status(200).json(theReview)}
+    if(err)                     {res.status(400).json(err)}
+    else if (!theReview)        {res.status(400).json({message: 'Review does not exist'})}
+    else                        {res.status(200).json(theReview)}
     });
 });
 
