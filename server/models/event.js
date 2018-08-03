ed86038034c21a5cf8ae79180ef937cebaafdf42
Junior A. Mendez M <junior_abigail@hotmail.com>
2018-08-03 15:52:11 -0400

    added Organization as a required field for the event creation

--- server/models/event.js
+++ server/models/event.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const eventSchema = new Schema({
-  participants:[{
-    user: {type: Schema.Types.ObjectId, ref: 'User'}, 
-    role: {type: String, enum: ['organizer', 'sponsor', 'performer', 'staff', 'security']}
-  }],
   organization: {type: Schema.Types.ObjectId, ref: 'Organization', required: true}, 
-  name: {type: String, required: true, unique: true},
-  type: {type: String, required: true},
-  location: {type: String, required: true},
-  startTime: {type: Date, required: true},
-  endTime: {type: Date, required: true},
-  status: {type: String, enum: ['confirmed', 'pending', 'cancelled']},
-  pictures: {type: String},
-  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
+  name:         {type: String, required: true, unique: true},
+  type:         {type: String, required: true},
+  location:     {type: String, required: true},
+  startTime:    {type: Date, required: true},
+  endTime:      {type: Date, required: true},
+  status:       {type: String, enum: ['confirmed', 'pending', 'cancelled']},
+  pictures:     {type: String},
+  reviews:      [{type: Schema.Types.ObjectId, ref: 'Review'}],
+  participants: [{user: {type: Schema.Types.ObjectId, ref: 'User'}, role: {type: String, enum: ['organizer', 'sponsor', 'performer', 'staff', 'security']}}]
 });
 
 const Event = mongoose.model('Event', eventSchema);
