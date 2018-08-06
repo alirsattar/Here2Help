ce2201a10ffb507a1be3311d07305b028080385c
Ali Sattar <alirsattar@gmail.com>
2018-08-06 10:50:36 -0400

    Cleaned up Event and Organization model files

--- server/models/event.js
+++ server/models/event.js
@@ -3,18 +3,18 @@ const Schema   = mongoose.Schema;
 
 const eventSchema = new Schema({
   participants:[{
-    user: {type: Schema.Types.ObjectId, ref: 'User'}, 
-    role: {type: String, enum: ['organizer', 'sponsor', 'performer', 'staff', 'security']}
-  }],
-  organization: {type: Schema.Types.ObjectId, ref: 'Organization', required: true}, 
-  name: {type: String, required: true, unique: true},
-  type: {type: String, required: true},
-  location: {type: String, required: true},
-  startTime: {type: Date, required: true},
-  endTime: {type: Date, required: true},
-  status: {type: String, enum: ['confirmed', 'pending', 'cancelled']},
-  pictures: {type: String},
-  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
+    user:         {type: Schema.Types.ObjectId, ref: 'User'}, 
+    role:         {type: String, enum: ['organizer', 'sponsor', 'performer', 'staff', 'security']}
+  }], 
+  organization:   {type: Schema.Types.ObjectId, ref: 'Organization',  required: true}, 
+  name:           {type: String,                                      required: true, unique: true},
+  type:           {type: String,                                      required: true},
+  location:       {type: String,                                      required: true},
+  startTime:      {type: Date,                                        required: true},
+  endTime:        {type: Date,                                        required: true},
+  status:         {type: String, enum: ['confirmed', 'pending', 'cancelled']},
+  pictures:       {type: String},
+  reviews:        [{type: Schema.Types.ObjectId, ref: 'Review'}]
 });
 
 const Event = mongoose.model('Event', eventSchema);
--- server/models/organization.js
+++ server/models/organization.js
@@ -1,5 +1,6 @@
 const mongoose    = require('mongoose');
 const Schema      = mongoose.Schema;
+
 const organizationSchema = new Schema({
   name:           {type: String, required: true, unique: true},
   address:        {type: String, required: true},
