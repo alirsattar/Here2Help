1151746104db77dc9e64bb0f891aa5ca57caa82c
Ali Sattar <alirsattar@gmail.com>
2018-08-07 08:03:02 -0400

    Added 'mainPhoto' properties and placeholder images to Event, Organization, and Review models (icons will be displayed if no mainPhoto)

--- server/models/user.js
+++ server/models/user.js
@@ -13,7 +13,7 @@ const userSchema = new Schema({
     password:             String,
     skills:               [String],
     preferredEventType:   [String],
-    profilePicture:       {type: String, default: 'https://vignette.wikia.nocookie.net/shingekinokyojin/images/c/c0/Anonymous_User.jpg/revision/latest?cb=20160312154300'},
+    profilePicture:       {type: String, default: '../public/images/user_placeholder.jpg'},
     phoneNumber:          Number,
     orgAdmin:             [{type: Schema.Types.ObjectId, ref: 'Organization', default: []}],
     events:               [{type: Schema.Types.ObjectId, ref: 'Event', default: []}],
